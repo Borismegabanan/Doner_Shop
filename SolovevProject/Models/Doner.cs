dc4e7935da57373d10a09edca953ecb09aa55924
Борис <krasilnikov.b@imatic.pro>
2020-03-28 19:17:55 +0300

    Added list of components

--- SolovevProject/Models/Doner.cs
+++ SolovevProject/Models/Doner.cs
@@ -10,5 +10,6 @@ namespace SolovevProject.Models
 		public int ID { get; set; }
 		public string Name { get; set; }
 		public int Price { get; set; }
+		public IEnumerable<Ingredients> Structure { get; set; }
 	}
 }
