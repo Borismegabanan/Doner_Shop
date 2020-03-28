24a6efe4dfe2739e6845e44f9e59734057ed220b
Борис <krasilnikov.b@imatic.pro>
2020-03-28 19:32:39 +0300

    Added Entity Framework

---
+++ SolovevProject/Models/DonerShopContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SolovevProject.Models
+{
+	public class DonerShopContext : DbContext
+	{
+		public DbSet<Ingredient> Ingredients { get; set; }
+		public DbSet<Doner> Doners { get; set; }
+
+	}
+}
