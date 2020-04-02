0a1947eeacf4658f2d08bf0942799f7f53ff3cec
Борис <krasilnikov.b@imatic.pro>
2020-04-02 15:50:07 +0300

    Initializer for my db (codeFirst)

---
+++ SolovevProject/Models/DonerDBInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SolovevProject.Models
+{
+	public class DonerDBInitializer:DropCreateDatabaseAlways<DonerShopContext>
+	{
+		protected override void Seed(DonerShopContext db)
+		{
+			db.Doners.Add(new Doner(){Name = "Обычная шаурма",Price = 100});
+			db.Doners.Add(new Doner() { Name = "Обычная шаурма с сыром", Price = 110 });
+			db.Doners.Add(new Doner() { Name = "Обычная шаурма с летним салатом", Price = 120 });
+			db.Doners.Add(new Doner() { Name = "Обычная шаурма с капустным салатом", Price = 115 });
+
+			base.Seed(db);
+		}
+	}
+}
