ae5b10ff28e02dffb9d5f467a955946cbe86d28d
Борис <krasilnikov.b@imatic.pro>
2020-04-19 12:34:34 +0300

    refactor

--- SolovevProject/Models/Doner.cs
+++ SolovevProject/Models/Doner.cs
@@ -7,7 +7,7 @@ namespace SolovevProject.Models
 {
 	public class Doner
 	{
-		public int ID { get; set; }
+		public int Id { get; set; }
 		public string Name { get; set; }
 		public int Price { get; set; }
 		public IEnumerable<Ingredient> Structure { get; set; }
